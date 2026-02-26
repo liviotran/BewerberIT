a67bbc3fd0f7a1d8cd89db55adf952d5061df8bf
Bewerber IT <bewerberIT@s7000.local>
2026-02-26 13:25:56 +0100

    Ich habe HalloWelt.java geschrieben

---
+++ 01-java-basics/05-meine-uebungen/HalloWelt1.java
@@ -0,0 +1,6 @@
+public class HalloWelt1 {
+    public static void main(String[] args) {
+        System.out.println("Hallo, mein name ist Vasihar!");
+         System.out.println("Hallo, mein Name ist Anna!");
+    }
+}
