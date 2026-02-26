103d46dfa69bdc76682fde92360e8f72408bee53
Bewerber IT <bewerberIT@s7000.local>
2026-02-26 13:49:11 +0100

    Ich habe MeineVariablen.java geschrieben

---
+++ 01-java-basics/05-meine-uebungen/MeineVariablen.java
@@ -0,0 +1,16 @@
+public class MeineVariablen {
+    public static void main(String[] args) {
+
+        // Variablen anlegen
+        String name = "Anna";
+        int alter = 20;
+        double groesse = 1.65;
+        boolean istStudent = true;
+
+        // Variablen ausgeben
+        System.out.println("Name: " + name);
+        System.out.println("Alter: " + alter);
+        System.out.println("Groesse: " + groesse);
+        System.out.println("Student: " + istStudent);
+    }
+}
